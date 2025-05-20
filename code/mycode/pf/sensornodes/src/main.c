c461ee3c80d78b388aa8b276c5987ee75acbba76
adnaan buksh <s4743556@student.uq.edu.au>
2025-05-20 21:55:28 +1000

    Sensor node complete

--- code/mycode/pf/sensornodes/src/main.c
+++ code/mycode/pf/sensornodes/src/main.c
@@ -1,14 +1,31 @@
-#include <zephyr/settings/settings.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/hci.h>
-#include <string.h> 
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/json/json.h>
+#include <string.h>
 
+#define TEMP_HUMIDITY_NODE DT_ALIAS(st_hts221)
+static const struct device *temp_humidity_dev = DEVICE_DT_GET_ONE(TEMP_HUMIDITY_NODE);
+static struct sensor_value temp, hum;
+
+struct SensorJSON {
+    int t;
+    int h;
+    int g;
+};
+
+static const struct json_obj_descr sensor_descr[] = {
+    JSON_OBJ_DESCR_PRIM(struct SensorJSON, t, JSON_TOK_NUMBER),
+    JSON_OBJ_DESCR_PRIM(struct SensorJSON, h, JSON_TOK_NUMBER),
+    JSON_OBJ_DESCR_PRIM(struct SensorJSON, g, JSON_TOK_NUMBER),
+};
 
 static const struct bt_data ad[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-    BT_DATA_BYTES(BT_DATA_UUID16_ALL,
-        0x0f, 0x18)  
+    BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0x0f, 0x18)  
 };
 
 static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
@@ -22,14 +39,48 @@ static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 	}
 	mac_addr[17] = '\0';
 
-	 
 }
 
+static void read_temp(void) {
+    if (!device_is_ready(temp_humidity_dev)) {
+        printk("Temp/Hum sensor not ready\n");
+        return;
+    }
+    if (sensor_sample_fetch(temp_humidity_dev) < 0) {
+        printk("Failed to fetch temp sample\n");
+        return;
+    }
+    if (sensor_channel_get(temp_humidity_dev,
+                           SENSOR_CHAN_AMBIENT_TEMP,
+                           &temp) < 0) {
+        printk("Failed to get temp channel\n");
+    }
+}
+
+static void read_hum(void) {
+    if (!device_is_ready(temp_humidity_dev)) {
+        printk("Temp/Hum sensor not ready\n");
+        return;
+    }
+    if (sensor_sample_fetch(temp_humidity_dev) < 0) {
+        printk("Failed to fetch hum sample\n");
+        return;
+    }
+    if (sensor_channel_get(temp_humidity_dev,
+                           SENSOR_CHAN_HUMIDITY,
+                           &hum) < 0) {
+        printk("Failed to get hum channel\n");
+    }
+}
+
+static int process_gas(void) {
+	//hamza code
+    return 0;
+}
 
 int main(void) {
 	int err; 
 	
-	/* Initialise the Bluetooth Subsystem */
 	err = bt_enable(NULL);
 	if (err) {
 		printk("Bluetooth init failed (err %d)\n", err);
@@ -45,13 +96,6 @@ int main(void) {
 		.window     = BT_GAP_SCAN_FAST_WINDOW,
 	};
 
-	err = bt_le_scan_start(&scan_param, device_found);
-	if (err) {
-		printk("Start scanning failed (err %d)\n", err);
-		return 0;
-	}
-	printk("Started scanning...\n");
-
 	/* Start advertising */
 	err = bt_le_adv_start(BT_LE_ADV_NCONN_IDENTITY, ad, ARRAY_SIZE(ad),
 		NULL, 0);
@@ -62,18 +106,45 @@ int main(void) {
 	}
 
 	while (1) {
-		// struct bt_data ad1[] = {
-		// 	BT_DATA_BYTES(BT_DATA_FLAGS,BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR),
-		// 	BT_DATA_BYTES(BT_DATA_MANUFACTURER_DATA,
-		// 			  //add data here)
-		// 	)
-		// };
+        read_temp();  
+        read_hum();    
 
-		// int data = bt_le_adv_update_data(ad1, ARRAY_SIZE(ad1), NULL, 0);
-		// if (data) {
-		// 	printk("(err %d)\n", data);
-		// }
+        int tvoc = process_gas();
 
-		// k_msleep(50);
-	}
+		struct SensorJSON data = {
+            .t = temp.val1,
+            .h = hum.val1,
+            .g = tvoc
+        };
+
+		char jsonBuf[128];
+        int ret = json_obj_encode_buf(sensor_descr,
+                                          ARRAY_SIZE(sensor_descr),
+                                          &data,
+                                          jsonBuf,
+                                          sizeof(jsonBuf));
+        if (ret != 0) {
+			printk("JSON error\n");
+		} else {
+			printk("%s\n", jsonBuf);
+		}
+
+        struct bt_data adv[] = {
+            BT_DATA_BYTES(BT_DATA_FLAGS,
+                          (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+            BT_DATA(BT_DATA_MANUFACTURER_DATA,
+                    (const uint8_t *)jsonBuf, (size_t)ret),
+        };
+
+        err = bt_le_adv_update_data(adv, ARRAY_SIZE(adv), NULL, 0);
+
+        if (err) {
+            printk("adv_update failed: %d\n", err);
+        } else {
+            printk("ADV -> %s\n", json);
+        }
+
+        // wait 0.1 second 
+        k_msleep(100);
+    }
 }
